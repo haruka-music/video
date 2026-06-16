f476352336d674abbf4627311ef5101aa29fe564
HARUKA <haruka.music.style@icloud.com>
2026-06-16 15:24:29 +0900

    completed videos

--- video/src/app/page.tsx
+++ video/src/app/page.tsx
@@ -1,3 +1,15 @@
 export default function Home() {
-  return <main></main>;
+  return (
+    <main>
+      <div style={{ padding: "20px" }}>
+        <h1>ローカル動画の再生【フルハウス】</h1>
+        <video
+          src="/video/Everywhere You Look-Wi-Fi（高速）.mp4"
+          width="100%"
+          controls
+          preload="metadata"
+        />
+      </div>
+    </main>
+  );
 }
